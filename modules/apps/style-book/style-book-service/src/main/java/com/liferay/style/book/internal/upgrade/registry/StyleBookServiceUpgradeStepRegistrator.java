5b6540deeb1ec32140c0169a98897d6a2a27306a
Thiago Buarque <thiago.buarque@liferay.com>
2025-04-09 15:23:26 -0300

    LPD-52966 add update process to update StyleBookEntryVersion themeId

--- modules/apps/style-book/style-book-service/src/main/java/com/liferay/style/book/internal/upgrade/registry/StyleBookServiceUpgradeStepRegistrator.java
+++ modules/apps/style-book/style-book-service/src/main/java/com/liferay/style/book/internal/upgrade/registry/StyleBookServiceUpgradeStepRegistrator.java
@@ -16,6 +16,8 @@ import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 import com.liferay.style.book.internal.upgrade.v1_1_0.StyleBookEntryUpgradeProcess;
 import com.liferay.style.book.internal.upgrade.v1_2_0.StyleBookEntryVersionUpgradeProcess;
 import com.liferay.style.book.internal.upgrade.v1_7_0.StyleBookEntryThemeIdUpgradeProcess;
+import com.liferay.style.book.internal.upgrade.v1_8_0.StyleBookEntryVersionThemeIdUpgradeProcess;
+import com.liferay.style.book.service.StyleBookEntryLocalService;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -90,6 +92,11 @@ public class StyleBookServiceUpgradeStepRegistrator
 			"1.6.0", "1.7.0",
 			new StyleBookEntryThemeIdUpgradeProcess(
 				_frontendTokenDefinitionRegistry, _groupLocalService));
+
+		registry.register(
+			"1.7.0", "1.8.0",
+			new StyleBookEntryVersionThemeIdUpgradeProcess(
+				_groupLocalService, _styleBookEntryLocalService));
 	}
 
 	@Reference
@@ -97,5 +104,8 @@ public class StyleBookServiceUpgradeStepRegistrator
 
 	@Reference
 	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private StyleBookEntryLocalService _styleBookEntryLocalService;
 
 }
---
+++ modules/apps/style-book/style-book-service/src/main/java/com/liferay/style/book/internal/upgrade/v1_8_0/StyleBookEntryVersionThemeIdUpgradeProcess.java
@@ -0,0 +1,106 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.style.book.internal.upgrade.v1_8_0;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.style.book.model.StyleBookEntry;
+import com.liferay.style.book.service.StyleBookEntryLocalService;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * @author Thiago Buarque
+ */
+public class StyleBookEntryVersionThemeIdUpgradeProcess extends UpgradeProcess {
+
+	public StyleBookEntryVersionThemeIdUpgradeProcess(
+		GroupLocalService groupLocalService,
+		StyleBookEntryLocalService styleBookEntryLocalService) {
+
+		_groupLocalService = groupLocalService;
+		_styleBookEntryLocalService = styleBookEntryLocalService;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		try (PreparedStatement preparedStatement1 = connection.prepareStatement(
+				"select groupId, ctCollectionId, styleBookEntryId, themeId " +
+					"from StyleBookEntryVersion");
+			PreparedStatement preparedStatement2 =
+				AutoBatchPreparedStatementUtil.autoBatch(
+					connection,
+					"update StyleBookEntryVersion set themeId = ? where " +
+						"ctCollectionId = ? and styleBookEntryId = ?");
+			PreparedStatement preparedStatement3 =
+				AutoBatchPreparedStatementUtil.autoBatch(
+					connection,
+					"delete from StyleBookEntryVersion where " +
+						"styleBookEntryId = ?");
+			PreparedStatement preparedStatement4 =
+				AutoBatchPreparedStatementUtil.autoBatch(
+					connection,
+					"delete from StyleBookEntry where styleBookEntryId = ?");
+			ResultSet resultSet = preparedStatement1.executeQuery()) {
+
+			while (resultSet.next()) {
+				long groupId = resultSet.getLong("groupId");
+				long styleBookEntryId = resultSet.getLong("styleBookEntryId");
+
+				Group group = _groupLocalService.fetchGroup(groupId);
+
+				if (group == null) {
+					if (_log.isWarnEnabled()) {
+						_log.warn(
+							StringBundler.concat(
+								"Unable to find group with id ", groupId,
+								". Removing orphaned style book entry version ",
+								"with id ", styleBookEntryId, "."));
+					}
+
+					preparedStatement3.setLong(1, styleBookEntryId);
+
+					preparedStatement3.addBatch();
+
+					preparedStatement4.setLong(1, styleBookEntryId);
+
+					preparedStatement4.addBatch();
+
+					continue;
+				}
+
+				StyleBookEntry styleBookEntry =
+					_styleBookEntryLocalService.fetchStyleBookEntry(
+						styleBookEntryId);
+
+				preparedStatement2.setString(1, styleBookEntry.getThemeId());
+
+				preparedStatement2.setLong(
+					2, resultSet.getLong("ctCollectionId"));
+				preparedStatement2.setLong(3, styleBookEntryId);
+
+				preparedStatement2.addBatch();
+			}
+
+			preparedStatement2.executeBatch();
+			preparedStatement3.executeBatch();
+			preparedStatement4.executeBatch();
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		StyleBookEntryVersionThemeIdUpgradeProcess.class);
+
+	private final GroupLocalService _groupLocalService;
+	private final StyleBookEntryLocalService _styleBookEntryLocalService;
+
+}
