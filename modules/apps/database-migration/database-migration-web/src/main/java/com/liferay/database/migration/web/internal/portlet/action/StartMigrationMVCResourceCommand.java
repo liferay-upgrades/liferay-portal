d62ee04b5ce2df8d6032c6d8bd4ede834aaccfbe
Albert Gomes <albert.gomes@liferay.com>
2026-07-07 22:04:22 -0300

    NOISSUE Add JSON resource endpoints for the database migration portlet

---
+++ modules/apps/database-migration/database-migration-web/src/main/java/com/liferay/database/migration/web/internal/portlet/action/StartMigrationMVCResourceCommand.java
@@ -0,0 +1,101 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.database.migration.web.internal.portlet.action;
+
+import com.liferay.database.migration.service.DatabaseMigrationManager;
+import com.liferay.database.migration.web.internal.constants.DatabaseMigrationPortletKeys;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import jakarta.portlet.ResourceRequest;
+import jakarta.portlet.ResourceResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Albert Gomes Cabral
+ */
+@Component(
+	property = {
+		"jakarta.portlet.name=" + DatabaseMigrationPortletKeys.DATABASE_MIGRATION,
+		"mvc.command.name=/database_migration/start_migration"
+	},
+	service = MVCResourceCommand.class
+)
+public class StartMigrationMVCResourceCommand extends BaseMVCResourceCommand {
+
+	@Override
+	protected void doServeResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws Exception {
+
+		String sourceJDBCURL = ParamUtil.getString(
+			resourceRequest, "sourceJDBCURL");
+		String sourceUserName = ParamUtil.getString(
+			resourceRequest, "sourceUserName");
+		String targetJDBCURL = ParamUtil.getString(
+			resourceRequest, "targetJDBCURL");
+		String targetUserName = ParamUtil.getString(
+			resourceRequest, "targetUserName");
+
+		String error = null;
+
+		if (Validator.isNull(sourceJDBCURL) ||
+			Validator.isNull(sourceUserName) ||
+			Validator.isNull(targetJDBCURL) ||
+			Validator.isNull(targetUserName)) {
+
+			error = "connectionInformationRequired";
+		}
+		else if (!StringUtil.toLowerCase(
+					targetJDBCURL
+				).contains(
+					"postgresql"
+				)) {
+
+			error = "targetDatabaseMustBePostgreSQL";
+		}
+		else {
+			try {
+				_databaseMigrationManager.startMigration(
+					sourceJDBCURL, sourceUserName,
+					ParamUtil.getString(resourceRequest, "sourcePassword"),
+					targetJDBCURL, targetUserName,
+					ParamUtil.getString(resourceRequest, "targetPassword"));
+			}
+			catch (IllegalStateException illegalStateException) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(illegalStateException);
+				}
+
+				error = "migrationAlreadyRunning";
+			}
+		}
+
+		JSONPortletResponseUtil.writeJSON(
+			resourceRequest, resourceResponse,
+			JSONUtil.put(
+				"error", error
+			).put(
+				"started", Validator.isNull(error)
+			));
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		StartMigrationMVCResourceCommand.class);
+
+	@Reference
+	private DatabaseMigrationManager _databaseMigrationManager;
+
+}
