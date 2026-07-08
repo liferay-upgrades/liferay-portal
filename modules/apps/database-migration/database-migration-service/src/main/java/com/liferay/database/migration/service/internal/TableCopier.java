141123e01874a81ce015028cd187a721470a58b3
Albert Gomes <albert.gomes@liferay.com>
2026-07-08 08:31:05 -0300

    NOISSUE Insert migration rows through the batch API to drop the format suppression

--- modules/apps/database-migration/database-migration-service/src/main/java/com/liferay/database/migration/service/internal/TableCopier.java
+++ modules/apps/database-migration/database-migration-service/src/main/java/com/liferay/database/migration/service/internal/TableCopier.java
@@ -10,6 +10,7 @@ import com.liferay.petra.io.unsync.UnsyncBufferedReader;
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.Base64;
@@ -107,7 +108,8 @@ public class TableCopier {
 			PreparedStatement selectPreparedStatement =
 				sourceConnection.prepareStatement(selectSQL);
 			PreparedStatement insertPreparedStatement =
-				targetConnection.prepareStatement(insertSQL)) {
+				AutoBatchPreparedStatementUtil.autoBatch(
+					targetConnection, insertSQL)) {
 
 			targetConnection.setAutoCommit(false);
 
@@ -129,7 +131,9 @@ public class TableCopier {
 								targetColumnTypes.get(columnName));
 						}
 
-						insertPreparedStatement.executeUpdate();
+						insertPreparedStatement.addBatch();
+
+						insertPreparedStatement.executeBatch();
 
 						targetConnection.releaseSavepoint(savepoint);
 
