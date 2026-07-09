85e353de6a27de5753675f8f93b0bbfb54f9cca1
Albert Gomes <albert.gomes@liferay.com>
2026-07-09 19:49:56 -0300

    NOISSUE Create DatabaseMigrationPortletKeys constants

---
+++ modules/apps/database-migration/database-migration-web/src/main/java/com/liferay/database/migration/web/internal/constants/DatabaseMigrationPortletKeys.java
@@ -0,0 +1,17 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.database.migration.web.internal.constants;
+
+/**
+ * @author Albert Gomes Cabral
+ */
+public class DatabaseMigrationPortletKeys {
+
+	public static final String DATABASE_MIGRATION =
+		"com_liferay_database_migration_web_internal_portlet_" +
+			"DatabaseMigrationPortlet";
+
+}
